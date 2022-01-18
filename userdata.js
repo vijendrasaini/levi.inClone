f54bb91a02c889614eab25babd8b699f0ed0cab1
shyam g <meghashyam.g@gmail.com>
2022-01-18 18:56:01 +0530

    login , signup & user data functions added -- pending(styling, header, footer integration)

---
+++ userdata.js
@@ -0,0 +1,86 @@
+let userDetails = [
+  {
+    email: "demo@levi.com",
+    password: 11112222,
+    phone: 9876543210,
+    cart: [],
+    orders: [],
+    status: false,
+  },
+];
+
+localStorage.setItem("users", JSON.stringify(userDetails));
+
+function signIn() {
+  let currentUser;
+  let userDetails = JSON.parse(localStorage.getItem("users"));
+
+  let siEmail = document.getElementById("si_email").value;
+  let siPassword = document.getElementById("si_password").value;
+  let i = 0;
+  let j = 0;
+  while (i < userDetails.length) {
+    if (userDetails[i].email == siEmail) {
+      j++;
+      if (userDetails[i].password == siPassword) {
+        j--;
+        userDetails[i].status = true;
+        alert("User Found!");
+        currentUser = userDetails[i];
+        localStorage.setItem("currentuser", JSON.stringify(currentUser));
+        // console.log(currentUser);
+        window.location.href = "home.html";
+        break;
+      } else {
+        alert("Password Incorrect!");
+        break;
+      }
+    }
+    if (j == 0) {
+      alert("User not found!");
+    }
+    i++;
+  }
+
+  //   console.log(siEmail);
+}
+
+function signUp() {
+  let userDetails = JSON.parse(localStorage.getItem("users"));
+
+  let suEmail = document.getElementById("suEmail").value;
+
+  let suPhone = document.getElementById("suPhone").value;
+  if (suPhone.length !== 10) {
+    alert("Enter valid mobile number");
+    return;
+  }
+  for (let i = 0; i < userDetails.length; i++) {
+    if (suEmail == userDetails[i].email) {
+      alert("Email already exists");
+      return;
+    }
+    if (suPhone == userDetails[i].phone) {
+      alert("Phone number already taken");
+      return;
+    }
+  }
+  let suPassword = document.getElementById("suPassword").value;
+  if (suPassword.length < 8) {
+    alert("Enter valid password");
+    return;
+  }
+  let newUser = {
+    email: suEmail,
+    password: suPassword,
+    phone: suPhone,
+    cart: [],
+    orders: [],
+    status: true,
+  };
+
+  userDetails.push(newUser);
+  alert("Account created successfully!");
+  window.location.href = "home.html";
+  localStorage.setItem("users", JSON.stringify(userDetails));
+}
