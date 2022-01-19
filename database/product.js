bfe6f2a8cd259343d6eb5178a4196deef9310578
shyam g <meghashyam.g@gmail.com>
2022-01-19 17:44:28 +0530

    del

--- database/product.js
+++ database/product.js
@@ -7,11 +7,17 @@ async function fetchData() {
   try {
     let data = await fetch(url);
     let response = await data.json();
-    obj = response;
+    obj = response.products;
     console.log(obj);
+    singleProd(obj);
   } catch (error) {
     console.log("error is:", error);
   }
 }
 
-// console.log(obj);
+function singleProd(array) {
+  let pro_path = document.getElementById("pro_path");
+
+  pro_path.textContent =
+    array[0].gender + "/ " + array[0].category + "/ " + array[0].type;
+}
