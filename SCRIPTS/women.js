b38b51b0e774c04e387909d92c9bd7fddd7279d1
VIJENDRA SAINI <docs.vijendra@gmail.com>
2022-01-22 17:38:42 +0530

    Sorting functionalities are added.

--- SCRIPTS/women.js
+++ SCRIPTS/women.js
@@ -44,7 +44,7 @@ let appendMenData = (arr) => {
         menDiv.append(image, title, priceDiv);
 
         menDiv.addEventListener('click',(e)=>{
-            showfullDetails(data,index)
+            showfullDetails(arr,index)
         })
         document.getElementById("menProductDiv").append(menDiv);
 
@@ -114,4 +114,62 @@ function sort1(i){
         }
     console.log(arr)
     appendMenData(arr)
+}
+
+const sortSelectButton = document.getElementById('sortSelect')
+sortSelectButton.onchange = ()=>{
+    const value = document.getElementById('sortSelect').value
+    sort2(value)
+}
+function sort2(v){
+    console.log(v)
+    if(v == 'high'){
+        data.sort((a,b)=>(b.price.price-a.price.price))
+        appendMenData(data)   
+    }
+    else if(v == 'low'){
+        data.sort((a,b)=>(a.price.price-b.price.price))
+        appendMenData(data)
+    }
+    else if(v == 'a'){
+        data.sort((a,b)=>{
+            if(a.title>b.title) return 1
+            if(a.title<b.title) return -1
+            return 0
+        })
+        appendMenData(data)
+    }
+    else{
+        data.sort((a,b)=>{
+            if(a.title>b.title) return -1
+            if(a.title<b.title) return 1
+            return 0
+        })
+        appendMenData(data)
+    }
+}
+
+const openAndCloseClass = document.getElementsByClassName('openAndClose')
+const openAndCloseDivClass = document.getElementsByClassName('openAndCloseDiv')
+for(let i=0;i<openAndCloseClass.length;i++)
+{
+    openAndCloseClass[i].style.cursor = "pointer"
+    openAndCloseClass[i].onclick = ()=>{
+        openAndCloseFunc(i)
+    }
+}
+function openAndCloseFunc(i)
+{
+    console.log("hi",i)
+    const contentp = `<i class="fas fa-plus-circle">`
+    const contentm = `<i class="fas fa-minus-circle">`
+    const more=document.getElementById("openAndCloseDiv"+i);
+    if(more.style.display==="none"){
+        more.style.display="block";
+        openAndCloseClass[i].innerHTML = contentp
+    }
+    else{
+        more.style.display="none";
+        openAndCloseClass[i].innerHTML = contentm
+    }
 }
