9bad917cb875f7d559f7130d0e9a2199b4ffd5cb
VIJENDRA SAINI <docs.vijendra@gmail.com>
2022-01-22 19:29:39 +0530

    Login page is linked to header section.

--- COMPONENTS/header.js
+++ COMPONENTS/header.js
@@ -6,7 +6,7 @@ const navbar = ()=>{
             <li><a href="./levihelp.html">HELP</a></li>
             <li><a href="">TRACK ORDER</a></li>
             <li><a href="./store_locator.html">STORE LOCATOR</a></li>
-            <li><a href="">SIGN IN</a></li>
+            <li><a href="./login.html">SIGN IN</a></li>
             <li><img src="https://media.istockphoto.com/vectors/flag-of-india-vector-id472317739?b=1&k=20&m=472317739&s=170667a&w=0&h=Ju9wPp7N7fM00TiZtVuVr_wc6elG1L-b32DMlY7JHc4=" alt=""></li>
         </ul>
     </div>
