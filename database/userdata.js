f9c445c62d9301360b9fdac9a3785559e30d2235
shyam g <meghashyam.g@gmail.com>
2022-01-22 10:49:43 +0530

    user data working through server

--- database/userdata.js
+++ database/userdata.js
@@ -1,108 +1,178 @@
-let userDetails = JSON.parse(localStorage.getItem("users"));
+// let userDetails = JSON.parse(localStorage.getItem("users"));
 
-let url1 = "http://127.0.0.1:7000/api/users";
+let url1 = "http://127.0.0.1:7000/api/allusers";
 
-// let obj;
-fetchuserData();
-
-async function fetchuserData() {
+async function signIn() {
   try {
     let data = await fetch(url1);
     let response = await data.json();
-    let obj = response;
-    console.log(obj);
-    // let single = [];
-    // single = obj[1];
-    // console.log(single);
-    // singleProd(single);
+    console.log(response);
+    let userDetails = response[0].users;
+    let currentUser = response[1].currentuser;
+
+    console.log(userDetails, currentUser);
+    if (userDetails.length === 0) {
+      alert("User not found!");
+    }
+
+    let siEmail = document.getElementById("si_email").value;
+    let siPassword = document.getElementById("si_password").value;
+    let i = 0;
+    let j = 0;
+    while (i < userDetails.length) {
+      if (userDetails[i].email == siEmail) {
+        j++;
+        if (userDetails[i].password == siPassword) {
+          userDetails[i].status = true;
+          alert("User Found!");
+          currentUser = userDetails[i];
+          console.log(currentUser);
+          updateCurUser();
+          async function updateCurUser() {
+            try {
+              const url = "http://127.0.0.1:7000/api/allusers/2";
+              const q = {
+                id: 2,
+                currentuser: currentUser,
+              };
+              console.log("here");
+              const arg = {
+                method: "PUT",
+                body: JSON.stringify(q),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              };
+              const response = await fetch(url, arg);
+              const result = await response.json();
+              console.log(result);
+              window.location.href = "index.html";
+            } catch (error) {
+              console.log(error);
+            }
+          }
+          // localStorage.setItem("currentuser", JSON.stringify(currentUser));
+
+          //---------->Redirect to Home page here<----------------
+
+          break;
+        } else {
+          alert("Password Incorrect!");
+          break;
+        }
+      }
+
+      i++;
+    }
+    if (j == 0) {
+      alert("User not found!");
+    }
   } catch (error) {
     console.log("error is:", error);
   }
 }
 
-console.log(userDetails);
+async function signUp() {
+  // console.log(userDetails);
+  try {
+    let data = await fetch(url1);
+    let response = await data.json();
+    let userDetails = response[0].users;
+    let currentUser = response[1].currentuser;
 
-function signIn() {
-  if (userDetails.length === 0) {
-    alert("User not found!");
-  }
+    console.log(userDetails, currentUser);
+    let suEmail = document.getElementById("suEmail").value;
 
-  let siEmail = document.getElementById("si_email").value;
-  let siPassword = document.getElementById("si_password").value;
-  let i = 0;
-  let j = 0;
-  while (i < userDetails.length) {
-    if (userDetails[i].email == siEmail) {
-      j++;
-      if (userDetails[i].password == siPassword) {
-        userDetails[i].status = true;
-        alert("User Found!");
-        let currentUser = userDetails[i];
-        console.log(currentUser);
-        localStorage.setItem("currentuser", JSON.stringify(currentUser));
-
-        // window.location.href = "index.html";
-        //---------->Redirect to Home page here<----------------
-
-        break;
-      } else {
-        alert("Password Incorrect!");
-        break;
+    let suPhone = document.getElementById("suPhone").value;
+    if (suEmail.indexOf("@") == -1 || suEmail.length === 0) {
+      alert("Enter valid Email id");
+      return;
+    }
+    if (suPhone.length !== 10) {
+      alert("Enter valid mobile number");
+      return;
+    }
+    for (let i = 0; i < userDetails.length; i++) {
+      if (suEmail == userDetails[i].email) {
+        alert("Email already exists");
+        return;
+      }
+      if (suPhone == userDetails[i].phone) {
+        alert("Phone number already taken");
+        return;
       }
     }
-
-    i++;
-  }
-  if (j == 0) {
-    alert("User not found!");
-  }
-}
-
-function signUp() {
-  // console.log(userDetails);
-
-  let suEmail = document.getElementById("suEmail").value;
-
-  let suPhone = document.getElementById("suPhone").value;
-
-  if (suPhone.length !== 10) {
-    alert("Enter valid mobile number");
-    return;
-  }
-  for (let i = 0; i < userDetails.length; i++) {
-    if (suEmail == userDetails[i].email) {
-      alert("Email already exists");
+    let suPassword = document.getElementById("suPassword").value;
+    if (suPassword.length < 8) {
+      alert("Enter valid password");
       return;
     }
-    if (suPhone == userDetails[i].phone) {
-      alert("Phone number already taken");
-      return;
+    let newUser = {
+      email: suEmail,
+      password: suPassword,
+      phone: suPhone,
+      cart: [],
+      orders: [],
+      status: true,
+    };
+
+    userDetails.push(newUser);
+    console.log(userDetails);
+    updateUsers();
+    async function updateUsers() {
+      try {
+        const url = "http://127.0.0.1:7000/api/allusers/1";
+        const o = {
+          id: 1,
+          users: userDetails,
+        };
+        console.log("here");
+        const arg = {
+          method: "PUT",
+          body: JSON.stringify(o),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await fetch(url, arg);
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
     }
+    updateCurUser();
+    async function updateCurUser() {
+      try {
+        const url = "http://127.0.0.1:7000/api/allusers/2";
+        const q = {
+          id: 2,
+          currentuser: currentUser,
+        };
+        console.log("here");
+        const arg = {
+          method: "PUT",
+          body: JSON.stringify(q),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await fetch(url, arg);
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    currentUser = newUser;
+    console.log(currentUser, userDetails);
+    alert("Account created successfully!");
+
+    // window.location.href = "index.html";
+    //--------->redirect to home page here<---------
+  } catch (error) {
+    console.log("error is:", error);
   }
-  let suPassword = document.getElementById("suPassword").value;
-  if (suPassword.length < 8) {
-    alert("Enter valid password");
-    return;
-  }
-  let newUser = {
-    email: suEmail,
-    password: suPassword,
-    phone: suPhone,
-    cart: [],
-    orders: [],
-    status: true,
-  };
-
-  userDetails.push(newUser);
-  let currentUser = newUser;
-  // console.log(currentUser);
-  localStorage.setItem("currentuser", JSON.stringify(currentUser));
-  alert("Account created successfully!");
-
-  // window.location.href = "index.html";
-  //--------->redirect to home page here<---------
-
-  localStorage.setItem("users", JSON.stringify(userDetails));
 }
 
 let modal = document.getElementById("forgot_popup");
